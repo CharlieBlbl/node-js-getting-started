ba1786311bd2d1f3913a9ce94781641d178f8406
CharlieBlbl <charlie.the.unicorn.blbl@gmail.com>
2016-06-06 15:40:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
+
